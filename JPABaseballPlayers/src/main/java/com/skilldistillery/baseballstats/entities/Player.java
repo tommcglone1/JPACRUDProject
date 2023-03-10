71a84f1deaea86637a1e1df2dde79d57ee81400b
Tom <tom.mcglone1@yahoo.com>
2023-03-10 16:33:41 -0500

    Configure Hibernate, test entity class

---
+++ JPABaseballPlayers/src/main/java/com/skilldistillery/baseballstats/entities/Player.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.baseballstats.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String team;
+	
+	public Player() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", team=" + team + "]";
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	
+	
+}
---
+++ JPABaseballPlayers/src/test/java/com/skilldistillery/baseballstats/entities/PlayerTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.baseballstats.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Player player;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPABaseballPlayers");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		player = em.find(Player.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		player =null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(player);
+		assertEquals("Bryce", player.getFirstName());
+		assertEquals("Harper", player.getLastName());
+		
+	}
+
+}
